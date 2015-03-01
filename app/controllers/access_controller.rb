6b900b73f6b993e3d608e4f824036c53f5de6899
Tim Licata <tim@timothylicata.com>
2015-02-28 20:13:00 -0800

    oops, signup for has fields for first_name, last_name, and email

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -59,7 +59,7 @@ class AccessController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password, :first_name, :last_name, :email)
   end
 
   def user_id
