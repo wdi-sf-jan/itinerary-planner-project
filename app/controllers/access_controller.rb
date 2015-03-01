be36996a3716a1ec8ccce8b9fd5ed4c32c6d349f
Tim Licata <tim@timothylicata.com>
2015-03-01 14:59:17 -0800

    move some user methods to application_controller

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -59,18 +59,4 @@ class AccessController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :password, :first_name, :last_name, :email)
   end
-
-  def user_id
-    session[:user_id]
-  end
-
-  def is_logged_in
-     user_id != nil
-  end
-
-  def current_user
-    if is_logged_in
-      user = User.find_by_id(user_id)
-    end
-  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+  def user_id
+    session[:user_id]
+  end
+
+  def is_logged_in
+    user_id != nil
+  end
+
+  def current_user
+    if is_logged_in
+      user = User.find_by_id(user_id)
+    end
+  end
 end
