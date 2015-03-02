e32c22260ba43cd64ae08951a21eab67f8ce2bff
Tim Licata <tim@timothylicata.com>
2015-03-01 23:16:40 -0800

    allow leaving reviews on waypoints

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -20,7 +20,11 @@ class ReviewsController < ApplicationController
       else
         flash[:alert] = "Review not saved"
       end
-      redirect_to reviewable
+      if itinerary != reviewable
+        redirect_to [itinerary, reviewable]
+      else
+        redirect_to itinerary
+      end
     else
       flash[:alert] = "An error occured"
       redirect_to root_path
